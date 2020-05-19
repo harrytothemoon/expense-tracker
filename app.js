420077381697467288195ea0bb9d32f09ad7fc64
Harry <harry811016@gmail.com>
2020-05-19 17:11:57 +0800

    feat: create file

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const routes = require('./routes')
+require('./config/mongoose')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+app.use(routes)
+
+app.listen(PORT, () => {
+  console.log(`App is running on port http://localhost:${PORT}`)
+})
