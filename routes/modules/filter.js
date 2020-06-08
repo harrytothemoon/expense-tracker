dd19f566c72e8214c7c1565440f61765921cf234
Harry <harry811016@gmail.com>
2020-06-09 02:01:19 +0800

    add month fiter

--- routes/modules/filter.js
+++ routes/modules/filter.js
@@ -40,4 +40,44 @@ router.get('/', (req, res) => {
   }
 })
 
+router.get('/month', (req, res) => {
+  const userId = req.user._id
+  const filter = req.query.filter
+  const amountFilter = Money.aggregate([
+    { $match: { userId: userId } },
+    {
+      $group: {
+        _id: { $month: "$date" },
+        amount: { $sum: "$amount" },
+      }
+    },
+    { $match: { _id: Number(filter) } },
+  ]).exec()
+  const recordFilter = Money.aggregate([
+    { $match: { userId: userId } },
+    {
+      $project: {
+        name: 1,
+        category: 1,
+        amount: 1,
+        merchant: 1,
+        date: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+        month: { $month: "$date" },
+        categoryIcon: 1,
+      }
+    },
+    { $match: { month: Number(filter) } },
+  ]).exec()
+  if (filter) {
+    Promise.all([amountFilter, recordFilter])
+      .then(([amountFilter, record]) => {
+        const totalamount = amountFilter[0]
+        res.render('index', { totalamount, record, filter })
+      })
+  } else {
+    Promise.all([amountFilter, recordFilter])
+      .then(() => res.redirect('/'))
+  }
+})
+
 module.exports = router
